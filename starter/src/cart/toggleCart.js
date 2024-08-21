5ce50d9171bef275276b25ca931b741e81a73544
Oscar <oscaromar89@yahoo.com>
2024-08-21 14:58:10 -0600

    se agrego la funcionabilidad que cierra y abre el carrito

--- starter/src/cart/toggleCart.js
+++ starter/src/cart/toggleCart.js
@@ -1,3 +1,15 @@
-import { getElement } from '../utils.js';
+import { getElement } from "../utils.js";
+
+const cartOverlay = getElement(".cart-overlay");
+const closeCartBtn = getElement(".cart-close");
+const toggleCartBtn = getElement(".toggle-cart");
+
+toggleCartBtn.addEventListener("click", function () {
+    cartOverlay.classList.add("show");
+});
+
+closeCartBtn.addEventListener("click", function () {
+    cartOverlay.classList.remove("show");
+});
 
 export const openCart = () => {};
