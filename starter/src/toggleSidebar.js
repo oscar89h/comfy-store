2ee303b2d2d6c5c2cc2dea1f3e5cd8a2facfbbbc
Oscar <oscaromar89@yahoo.com>
2024-08-20 23:05:27 -0600

    se agrego la funcionabilidad que abre y cierra el sidebar

--- starter/src/toggleSidebar.js
+++ starter/src/toggleSidebar.js
@@ -1,1 +1,16 @@
-import { getElement } from './utils.js';
+import { getElement } from "./utils.js";
+
+const sidebarOverlayCloseBtn = getElement(".sidebar-close");
+const sidebarOverlayEl = getElement(".sidebar-overlay");
+const toggleNavEl = getElement(".toggle-nav");
+
+sidebarOverlayCloseBtn.addEventListener("click", closeXBtn);
+toggleNavEl.addEventListener("click", openSidebar);
+
+function closeXBtn() {
+    this.parentElement.parentElement.classList.remove("show");
+}
+
+function openSidebar() {
+    sidebarOverlayEl.classList.add("show");
+}
